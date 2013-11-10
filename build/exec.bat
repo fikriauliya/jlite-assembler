f0a9b3f6e6710ad428b3a32e6a7979f78d5cb6be
unknown <LPTK>
2013-11-10 18:28:04 +0800

    minor change

--- build/exec.bat
+++ build/exec.bat
@@ -1,7 +1,8 @@
 @echo off
 REM rez.exe ../our_tests/very_simple.txt
-rez.exe ../our_tests/test_ops.txt
+REM rez.exe ../our_tests/test_ops.txt
 REM rez.exe ../tests/test_ops.txt
+rez.exe ../tests/test_booleans.txt
 REM rez.exe ../ex/02.txt
 REM rez.exe ../ex/01.txt > ../ex/01_out.txt
 echo.
