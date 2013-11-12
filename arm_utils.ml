daf1711d67dadc233024de5ea1aad46cc92e468a
unknown <LPTK>
2013-11-12 15:02:53 +0800

    minor refactoring

--- arm_utils.ml
+++ arm_utils.ml
@@ -24,6 +24,19 @@ module Id3Set = Set.Make(
 )
 
 
+let string_of_ralloc ((reg, var): reg_allocation): string option =
+  match !var with
+    | Some v -> Some (reg ^ "=" ^ v)
+    | None -> None
+
+let string_of_rallocs (rallocs: reg_allocations) (sep: string): string =
+  (*string_of_list rallocs string_of_ralloc sep*)
+  String.concat sep (List.flatten
+    (List.map (fun s -> match string_of_ralloc s with Some s -> [s] | _ -> []) rallocs)
+  )
+
+
+
 (*
 (* Takes the first n element in a list and returns two list: those elements and the remaining ones *)
 let rec vertical_split n ls =
@@ -75,57 +88,5 @@ let store_variable (stack_frame: type_layout) (src_reg: reg) (var_name: id3): ar
   let offset = get_variable_offset stack_frame var_name in
   let str = STR("", "", src_reg, RegPreIndexed("fp", offset, false)) in
   [str]
-
-let var_of_register (rallocs: reg_allocations) (r: reg): (id3 option) =
-  try
-    let (_, id3) = List.find (fun (reg_name, _) -> reg_name = r) rallocs in !id3
-  with
-  | Not_found -> failwith ("var_of_register: Invalid register name: " ^ r)
-
-let register_of_var (rallocs: reg_allocations) (v: id3): (reg option) =
-  try
-    let (reg, _) = List.find (fun (_, var) -> match !var with
-    | Some var_name -> var_name = v
-    | None -> false
-    ) rallocs in
-    (Some reg)
-  with
-  | Not_found -> None
-
-
-(* Update content of r to be new_var *)
-let update_rallocs_var_at_reg (rallocs: reg_allocations) (new_var: id3 option) (r: reg) =
-  try
-    let (_, var_option) = List.find (fun (reg_name, _) -> reg_name = r) rallocs in
-    let _ = (var_option := new_var) in ()
-  with
-  | Not_found -> failwith ("update_rallocs_var_at_reg: Invalid register name: " ^ r)
-
-
-let mtd_regs = ["a1";"a2";"a3";"a4"(*;"lr"*)]
-let reset_mtd_reg(*s*) rallocs =
-  (*
-  let _ = update_rallocs_var_at_reg rallocs (None) "a1" in
-  let _ = update_rallocs_var_at_reg rallocs (None) "a2" in
-  let _ = update_rallocs_var_at_reg rallocs (None) "a3" in
-  let _ = update_rallocs_var_at_reg rallocs (None) "a4" in
-  let _ = update_rallocs_var_at_reg rallocs (None) "lr" in
-  *)
-  let _ = map (update_rallocs_var_at_reg rallocs (None)) mtd_regs in
-  ()
-
-
-let request_method_call_reg (stack_frame: type_layout) (rallocs: reg_allocations) (r: reg) =
-  match var_of_register rallocs r with
-  | Some v ->
-    (* Some other variable exists in a_x register, spill and load *)
-    (*spill_variable stack_frame r rallocs*)
-    store_variable stack_frame r v
-  | None ->
-    (* No other variable exists in a_x register, just load *)
-    []
-
-let request_method_call_regs stack_frame rallocs : 'a list =
-  flatten (map (request_method_call_reg stack_frame rallocs) mtd_regs)
 
 
